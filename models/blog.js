84c697c379c6369427baf2547e186ec5c376b40a
cristian <cristiancocabjn@gmail.com>
2025-09-01 01:35:46 -0400

    exercise: 4.1 - 4.2 (project structure)

---
+++ models/blog.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'title missing']
+    },
+    author: {
+        type: String,
+        required: [true, 'author missing']
+    },
+    url: {
+        type: String,
+        required: [true, 'url missing']
+    },
+    likes: {
+        type: Number,
+        default: "0"
+    }
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
