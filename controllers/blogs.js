dfc22dd979d1418afb6a53d119c9db967609a209
cristian <cristiancocabjn@gmail.com>
2025-09-06 19:33:59 -0400

    exercise: 4.8 step: 1 (supertest - all blogs)

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,21 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response, next) => {
-    Blog.find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-        .catch(error => next(error))
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
+})
+
+blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
     const blog = new Blog ({
@@ -19,11 +25,8 @@ blogsRouter.post('/', (request, response, next) => {
         likes: body.likes
     })
 
-    blog.save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-        .catch(error => next(error))
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
---
+++ tests/api.test.js
@@ -0,0 +1,40 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const assert = require('node:assert')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    for (let blog of helper.initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    }
+})
+
+test('all blogs all returned', async () => {
+    const blogs = await helper.blogsInDb()
+
+    assert.strictEqual(blogs.length, helper.initialBlogs.length)
+})
+
+test.only('a specific title can be viewed', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+
+    const blogToView = blogsAtStart[0]
+
+    const resultNote = await api
+        .get(`/api/blogs/${blogToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    assert.deepStrictEqual(resultNote.body, blogToView)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
